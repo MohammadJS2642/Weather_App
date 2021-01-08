6997f4f27dfb73b4fb0a3b699eed3fe70f0d682e
MohammadJS2642 <mohammadjs2642@gmail.com>
2021-01-08 11:28:26 +0330

    import local_settings.py to this

--- Weather_App/Weather_App/settings.py
+++ Weather_App/Weather_App/settings.py
@@ -118,3 +118,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = '/static/'
+
+
+# for import some personal settings
+try:
+    from local_settings import *
+except ImportError:
+    pass
