36f4813aef273f55014e66f79d31e140b2224576
MohammadJS2642 <mohammadjs2642@gmail.com>
2021-01-23 16:30:14 +0330

    some change but not ok

--- Weather_App/CheckWeather/views.py
+++ Weather_App/CheckWeather/views.py
@@ -12,7 +12,10 @@ def ShowWeather(request):
     if request.method == "GET":
 
         # using from GET
-        cityName = request.GET['cityname']
+        cityName = request.method.get['cityname']
+
+        if cityName == '':
+            return HttpResponse("Not city")
 
         # this is api url for request to that
         city = cityName
@@ -20,7 +23,7 @@ def ShowWeather(request):
         url = f"http://api.openweathermap.org/data/2.5/weather?q={city}&appid={Api_Key}"
 
         # get string (json) from url
-        response = requests.get(url)
+        response = request.get(url)
 
         # convert that string to json
         check = response.json()
