1428ae47412f9d613e0edf206ab003a855fda7eb
MohammadJS2642 <mohammadjs2642@gmail.com>
2021-01-24 23:10:37 +0330

    set name in urls to lowercase

--- Weather_App/CheckWeather/urls.py
+++ Weather_App/CheckWeather/urls.py
@@ -2,6 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('showweather/<cityname>/', views.ShowWeather, name="ShowWeather"),
-    path('showweather/', views.ShowWeather, name="ShowWeather"),
+    path('showweather/<cityname>/', views.ShowWeather, name="showweather"),
+    path('showweather/', views.ShowWeather, name="showweather"),
 ]
