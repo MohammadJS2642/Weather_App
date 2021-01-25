608f890d865b264a1137c05d02619fcb841a9fb9
MohammadJS2642 <mohammadjs2642@gmail.com>
2021-01-25 23:16:26 +0330

    add some urls for test

--- Weather_App/CheckWeather/urls.py
+++ Weather_App/CheckWeather/urls.py
@@ -4,4 +4,7 @@ from . import views
 urlpatterns = [
     path('showweather/<cityname>/', views.ShowWeather, name="showweather"),
     path('showweather/', views.ShowWeather, name="showweather"),
+    path('showweather2/<cityname>/', views.ShowWeather2, name="ShowWeather2"),
+    path('showweather2/', views.ShowWeather2, name="ShowWeather2"),
+    path('ShowMe/', views.ShowMe, name="ShowMe"),
 ]
