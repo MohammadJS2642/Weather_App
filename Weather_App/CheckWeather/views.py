9ede45c60715f7468e6f680a21728fc27ff3184e
MohammadJS2642 <mohammadjs2642@gmail.com>
2021-01-25 23:16:54 +0330

    improve some codes in this

--- Weather_App/CheckWeather/views.py
+++ Weather_App/CheckWeather/views.py
@@ -22,6 +22,46 @@ def ShowWeather(request, cityname=''):
         # convert that string to json
         check = response.json()
 
+    elif check['cod'] == '404':
+        return HttpResponse("City is not true")
+
+    # if city not found
+    else:
+        return HttpResponse("City is not true")
+
+    # show json like string in html with httpresponse without template
+    # return HttpResponse(response)
+
+    check2 = check['weather']
+    context = {
+        'check': check2,
+    }
+
+    return render(request, 'CheckWeather/index.html', context)
+
+    # display a pice of  from json
+    # return HttpResponse(check['weather'])
+
+
+def ShowWeather2(request, cityname=''):
+
+    # for check what method is called
+    if request.method == "GET":
+
+        # this is api url for request to that
+        city = cityname
+
+        url = f"http://api.openweathermap.org/data/2.5/weather?q={city}&appid={Api_Key}"
+
+        # get string (json) from url
+        response = requests.get(url)
+
+        # convert that string to json
+        check = response.json()
+
+    elif check['cod'] == '404':
+        return HttpResponse("City is not true")
+
     # if city not found
     else:
         return HttpResponse("City is not true")
@@ -29,5 +69,16 @@ def ShowWeather(request, cityname=''):
     # show json like string in html with httpresponse without template
     return HttpResponse(response)
 
+    # check2 = check['weather']
+    # context = {
+    #     'check': check2,
+    # }
+
+    # return render(request, 'CheckWeather/index.html', context)
+
     # display a pice of  from json
     # return HttpResponse(check['weather'])
+
+
+def ShowMe(request):
+    return render(request, 'checkweather/index2.html')
