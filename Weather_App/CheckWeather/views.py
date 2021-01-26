6650eef9be157297e9a41dd66a7f127a5deeefde
MohammadJS2642 <mohammadjs2642@gmail.com>
2021-01-26 23:04:52 +0330

    for managing city exist or not two if added

--- Weather_App/CheckWeather/views.py
+++ Weather_App/CheckWeather/views.py
@@ -1,20 +1,20 @@
 from django.shortcuts import render, redirect
 from django.http import HttpResponse
 import requests
+import json
 from Weather_App.local_settings import Api_Key
 
+
 # Create your views here.
-
-
 def ShowWeather(request, cityname=''):
 
     # for check what method is called
     if request.method == "GET":
-
+        query = request.GET.get('cityname')
         # this is api url for request to that
         city = cityname
 
-        url = f"http://api.openweathermap.org/data/2.5/weather?q={city}&appid={Api_Key}"
+        url = f"http://api.openweathermap.org/data/2.5/weather?q={query}&appid={Api_Key}"
 
         # get string (json) from url
         response = requests.get(url)
@@ -22,25 +22,37 @@ def ShowWeather(request, cityname=''):
         # convert that string to json
         check = response.json()
 
-    elif check['cod'] == '404':
-        return HttpResponse("City is not true")
+        if check['cod'] == '400':
+            context = {
+                'not': 'Please set city!!',
+            }
+            return render(request, 'CheckWeather/index.html', context)
+
+        elif check['cod'] == '404':
+            context = {
+                'not': 'City Not Found!!',
+            }
+            return render(request, 'CheckWeather/index.html', context)
+
+        else:
+            weather = check['name']
+            context = {
+                'weather': weather,
+            }
+
+            return render(request, 'CheckWeather/index.html', context)
+
+    # --------------If everything is ok remove this--------------
+    # elif check['cod'] == '404':
+    #     return HttpResponse("City is not true")
 
     # if city not found
-    else:
-        return HttpResponse("City is not true")
+    # else:
+    #     return HttpResponse("City is not true")
 
     # show json like string in html with httpresponse without template
     # return HttpResponse(response)
-
-    check2 = check['weather']
-    context = {
-        'check': check2,
-    }
-
-    return render(request, 'CheckWeather/index.html', context)
-
-    # display a pice of  from json
-    # return HttpResponse(check['weather'])
+    # --------------If everything is ok remove this--------------
 
 
 def ShowWeather2(request, cityname=''):
