5861228dd9a4b47abe4a11b85ff24a4586b87907
MohammadJS2642 <mohammadjs2642@gmail.com>
2021-02-07 12:41:20 +0330

    improve context for show weather description and temp

--- Weather_App/CheckWeather/views.py
+++ Weather_App/CheckWeather/views.py
@@ -35,24 +35,51 @@ def ShowWeather(request, cityname=''):
                 return render(request, 'CheckWeather/index.html', context)
 
             else:
-                weather = check['name']
+                # **************************
+                # search inside weather arrey
+                weather_list = check['weather'][0]
+
+                # searching inside main dictionary
+                main = check['main']
+
+                # wind
+                wind = check['wind']
+                # **************************
+
+                # find main in weather status
+                status = weather_list['main']
+
+                # find weather description
+                description = weather_list['description']
+
+                # Weather Icon
+                weather_icon = weather_list['icon']
+
+                # temp in main
+                temp_kelvin = main['temp']
+                temp = round((temp_kelvin-273.15),1)
+
+                # speed wind
+                speed_wind = wind['speed']
+
+                # degree wind
+                degree_wind = wind['deg']
+
+                # city name
+                name = check['name']
+
                 context = {
-                    'weather': weather,
+                    'name': name,
+                    'status': status,
+                    'description': description,
+                    'weather_icon': weather_icon,
+                    'temp': temp,
+                    'speed_wind': speed_wind,
+                    'degree_wind': degree_wind,
                 }
 
                 return render(request, 'CheckWeather/index.html', context)
 
-        # --------------If everything is ok remove this--------------
-        # elif check['cod'] == '404':
-        #     return HttpResponse("City is not true")
-
-        # if city not found
-        # else:
-        #     return HttpResponse("City is not true")
-
-        # show json like string in html with httpresponse without template
-        # return HttpResponse(response)
-        # --------------If everything is ok remove this--------------
     except Exception as e:
         return render(request, 'checkweather/index.html', e)
 
@@ -80,6 +107,33 @@ def ShowWeather2(request, cityname=''):
     else:
         return HttpResponse("City is not true")
 
+    # search inside weather arrey
+    weather_list = check['weather'][0]
+
+    # searching inside main dictionary
+    main = check['main']
+
+    # find main in weather status
+    status = weather_list['main']
+
+    # find weather description
+    description = weather_list['description']
+
+    # Weather Icon
+    weather_icon = weather_list['icon']
+
+    # temp in main
+    temp = main['temp']
+
+    # wind
+    wind = check['wind']
+
+    # speed wind
+    speed_wind = wind['speed']
+
+    # degree wind
+    degree_wind = wind['deg']
+
     # show json like string in html with httpresponse without template
     return HttpResponse(response)
 
@@ -95,4 +149,4 @@ def ShowWeather2(request, cityname=''):
 
 
 def ShowMe(request):
-    return render(request, 'checkweather/index2.html')
+    return render(request, 'checkweather/index.html')
